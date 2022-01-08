9e8372110a5ee24de6f4817f93032004ebbcc177
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-01-08 07:29:34 -0500

    new

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # piedmontite
-A test server for GSI Vector Consumption in ArcGIS Online
+A test server for GSI Maps Vector Consumption in ArcGIS Online
+
+# Summary
+* This server does not delivers vector tiles (pbf).
+* This server delivers static htdocs contents in "htdocs" folder.
+* This server now has an interface with Esri ArcGIS online.
+    * It returns the index.json of the vector tile service. 
+    * It returns the style.json for the styling. 
+    * It retuns the simplified tilemap informaiton.
+
+# Tested environment
+* OS CentOS Linux release 7.8.2003
+* nodejs v16.13.1
+* npm v8.1.2
+
+# Vector Tile Information
+* Tile URL: `https://cyberjapandata.gsi.go.jp/xyz/{t}/{z}/{x}/{y}.pbf`
+* Zoom levels: ZL 4-17
+
+
+# References
+* https://github.com/gsi-cyberjapan/gsimaps-vector-experiment
---
+++ app.js
@@ -0,0 +1,52 @@
+const config = require('config')
+const express = require('express')
+//const spdy = require('spdy') //for https
+const cors = require('cors') 
+const morgan = require('morgan')
+const winston = require('winston')
+const DailyRotateFile = require('winston-daily-rotate-file')
+
+// config constants
+const morganFormat = config.get('morganFormat')
+const htdocsPath = config.get('htdocsPath')
+//const privkeyPath = config.get('privkeyPath') //for https
+//const fullchainPath = config.get('fullchainPath') //for https
+const port = config.get('port') 
+const logDirPath = config.get('logDirPath')
+
+// logger configuration
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new DailyRotateFile({
+      filename: `${logDirPath}/server-%DATE%.log`,
+      datePattern: 'YYYY-MM-DD'
+    })
+  ]
+})
+
+logger.stream = {
+  write: (message) => { logger.info(message.trim()) }
+}
+
+// app
+const app = express()
+var esriIFRouter = require('./routes/esriIF') //esri interface (tilemap, etc..)
+app.use(cors())
+app.use(morgan(morganFormat, {
+  stream: logger.stream
+}))
+app.use(express.static(htdocsPath))
+app.use('/esriIF', esriIFRouter) //esri interface
+
+//for http
+app.listen(port, () => {
+    console.log(`Running at Port ${port} ...`)
+})
+
+/* for https
+spdy.createServer({
+  key: fs.readFileSync(privkeyPath),
+  cert: fs.readFileSync(fullchainPath)
+}, app).listen(port)
+*/
