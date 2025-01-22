2fac74aca21d89eade9623a477fa87747f5124d1
k96mz <astrovation@gmail.com>
2025-01-22 14:12:42 -0500

    Initial commit

---
+++ apps.js
@@ -0,0 +1,63 @@
+const config = require("config");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const winston = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
+
+// config constants
+const morganFormat = config.get("morganFormat");
+const htdocsPath = config.get("htdocsPath");
+const port = config.get("port");
+const logDirPath = config.get("logDirPath");
+
+// logger configuration
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new DailyRotateFile({
+      filename: `${logDirPath}/server-%DATE%.log`,
+      datePattern: "YYYY-MM-DD",
+    }),
+  ],
+});
+
+logger.stream = {
+  write: (message) => {
+    logger.info(message.trim());
+  },
+};
+
+// app
+const app = express();
+var VTRouter = require("./routes/VT"); //tiling
+var esriIFRouter = require("./routes/esriIF"); //esri interface (tilemap, etc..)
+app.use(cors());
+app.use(
+  morgan(morganFormat, {
+    stream: logger.stream,
+  })
+);
+app.use(express.static(htdocsPath));
+app.use("/VT", VTRouter);
+app.use("/esriIF", esriIFRouter); //esri interface
+
+//for http
+// app.listen(port, () => {
+//     console.log(`Running at Port ${port} ...`)
+// })
+
+// for https
+const fs = require("fs");
+const spdy = require("spdy"); //for https
+const privkeyPath = config.get("privkeyPath"); //for https
+const fullchainPath = config.get("fullchainPath"); //for https
+spdy
+  .createServer(
+    {
+      key: fs.readFileSync(privkeyPath),
+      cert: fs.readFileSync(fullchainPath),
+    },
+    app
+  )
+  .listen(port);
